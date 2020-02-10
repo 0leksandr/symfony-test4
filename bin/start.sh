9a3e8970c2f6564e4a8fab86cb15e7997f9cd3e3
Oleksandr Boiko <oleksandr.boiko@i.ua>
2020-02-10 23:27:24 +0200

    fix adding user, moved composer and yarn to up
    Took 10 minutes

--- bin/start.sh
+++ bin/start.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-composer install
-yarn install
 #docker-compose --file docker/docker-compose.yml up --build --abort-on-container-exit
 docker-compose --file docker/docker-compose.yml up --build -d
 docker exec -it test4-php ./bin/up.sh
--- bin/up.sh
+++ bin/up.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+composer install && \
+yarn install && \
+"$(dirname "$0")"/symfony.sh self:update -y && \
 "$(dirname "$0")"/parallel_commands.sh "$(dirname "$0")/symfony.sh serve" "yarn encore dev --watch"
 #yarn encore dev --watch
