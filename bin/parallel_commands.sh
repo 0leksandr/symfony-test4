ed15bd27cac1b48163205561c1025272f251b03c
Oleksandr Boiko <oleksandr.boiko@i.ua>
2020-03-05 21:41:05 +0200

    `parallel` all-or-nothing; revoke sudo rights
    Took 1 hour 9 minutes

--- bin/parallel_commands.sh
+++ bin/parallel_commands.sh
@@ -1,12 +1,48 @@
 #!/bin/sh
-LIST2KILL=""
-LIST2WAIT=""
-for cmd in "$@"; do {
-  eval "$cmd" & pid=$!
-  LIST2WAIT="$LIST2WAIT $pid"
-  LIST2KILL="$LIST2KILL pkill -P $pid;"
-} done
+nr_args=$#
+all_or_nothing=""
+if [ "$1" = "-n" ]; then
+    nr_args=$((nr_args - 1))
+    all_or_nothing="-n"
+fi
 
-trap "$LIST2KILL" INT TERM
+if [ $nr_args -eq 1 ]; then
+    for arg in "$@"; do
+        if [ "$arg" != "-n" ]; then
+            commands="$(echo "$arg" |awk 'NF > 0' |sed -r "s ' \\' g" |sed -r "s ^.*\$ \'\0\' ")"
+            if [ "$(echo "$commands" |wc -l)" -gt 1 ]; then
+                eval "$0 $all_or_nothing $(echo "$commands" |tr '\n' ' ')"
+                exit 1
+            fi
+        fi
+    done
+fi
 
-wait $LIST2WAIT
+list2kill=""
+list2wait=""
+
+for cmd in "$@"; do
+    if [ "$cmd" != "-n" ]; then
+        eval "$cmd" & pid=$!
+        list2wait="$list2wait $pid"
+        list2kill="$list2kill pkill -P $pid;"
+    fi
+done
+
+trap '$list2kill' INT TERM
+
+if [ "$all_or_nothing" ]; then
+    # bash -c "wait -n"
+    while true; do
+        for pid in $list2wait; do
+#            if ! ps --pid "$pid" > /dev/null; then
+            if ! ps |grep -E -q "^ *$pid "; then
+                eval "$list2kill"
+                exit 1
+            fi
+        done
+        sleep 0.1
+    done
+else
+    wait $list2wait
+fi
--- bin/up.sh
+++ bin/up.sh
@@ -2,5 +2,7 @@
 composer install && \
 yarn install && \
 "$(dirname "$0")"/symfony.sh self:update -y && \
-"$(dirname "$0")"/parallel_commands.sh "$(dirname "$0")/symfony.sh serve" "yarn encore dev --watch"
-#yarn encore dev --watch
+"$(dirname "$0")"/parallel_commands.sh -n "
+    $(dirname "$0")/symfony.sh serve
+    yarn encore dev --watch
+"
