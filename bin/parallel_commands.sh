2574678df0b6e8f04ceabeadcfbe07e761cfeeb2
Oleksandr Boiko <oleksandr.boiko@i.ua>
2020-03-05 22:07:52 +0200

    `parallel`: ignore commented lines
    Took 15 minutes

--- bin/parallel_commands.sh
+++ bin/parallel_commands.sh
@@ -9,7 +9,7 @@ fi
 if [ $nr_args -eq 1 ]; then
     for arg in "$@"; do
         if [ "$arg" != "-n" ]; then
-            commands="$(echo "$arg" |awk 'NF > 0' |sed -r "s ' \\' g" |sed -r "s ^.*\$ \'\0\' ")"
+            commands="$(echo "$arg" |sed -r 's/^ *#.*$//' |awk 'NF > 0' |sed -r "s ^.*\$ '\0' ")"
             if [ "$(echo "$commands" |wc -l)" -gt 1 ]; then
                 eval "$0 $all_or_nothing $(echo "$commands" |tr '\n' ' ')"
                 exit 1
@@ -29,13 +29,13 @@ for cmd in "$@"; do
     fi
 done
 
-trap '$list2kill' INT TERM
+trap "$list2kill" INT TERM
 
 if [ "$all_or_nothing" ]; then
     # bash -c "wait -n"
     while true; do
         for pid in $list2wait; do
-#            if ! ps --pid "$pid" > /dev/null; then
+#            if ! ps --pid="$pid" > /dev/null; then
             if ! ps |grep -E -q "^ *$pid "; then
                 eval "$list2kill"
                 exit 1
